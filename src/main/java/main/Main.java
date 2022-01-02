f98ffae61a0946af0bab47745ff8b658ed108a47
pimmassink <pimmassink@gmail.com>
2022-01-02 17:20:46 +0100

    Updated main method

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -31,6 +31,27 @@ public class Main {
         Duration tripToPlutoHighSpeed = spaceMathService.calculateTripDuration(Acceleration.G(1), Distance.AU(40));
         System.out.println("A trip to Pluto at 1G takes: " + tripToPlutoHighSpeed.toString());
 
+
+        Duration tripToPlutoLowSpeed2 = spaceMathService.calculateTripDurationWithoutBreaking(Acceleration.Mss(3), Distance.AU(40));
+        System.out.println("A SINGLE trip to Pluto at 3MSS takes: " + tripToPlutoLowSpeed2.toString());
+
+        Duration tripToPlutoHighSpeed2 = spaceMathService.calculateTripDurationWithoutBreaking(Acceleration.Mss(10), Distance.AU(40));
+        System.out.println("A SINGLE trip to Pluto at 10MSS takes: " + tripToPlutoHighSpeed2.toString());
+
+
+        Duration t1 = spaceMathService.calculateTripDuration(Acceleration.Mss(10), Distance.Meters(125));
+        System.out.println("A trip at 3MSS takes: " + t1.toString());
+
+        Duration t2 = spaceMathService.calculateTripDuration(Acceleration.Mss(30), Distance.Meters(125));
+        System.out.println("A trip at 10MSS takes: " + t2.toString());
+
+        Distance d1 = spaceMathService.distanceTravelled(Acceleration.Mss(10), Duration.Weeks(2));
+        Distance d2 = spaceMathService.distanceTravelled(Acceleration.Mss(30), Duration.Weeks(2));
+
+        System.out.println("d1: " + d1);
+        System.out.println("d2: " + d2);
+
+
         // BELANGRIJK:
         //        https://en.wikipedia.org/wiki/Lorentz_transformation
         //        v2/c2
